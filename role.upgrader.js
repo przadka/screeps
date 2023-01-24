32c9c92b14a90b79bf12e24f46cc9f32925f3a08
Michal Przadka <przadka@gmail.com>
2023-01-24 16:36:15 +0100

    Improvements in upgrader code

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,24 +1,26 @@
 var roleUpgrader = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
+    run: function (creep) {
 
         if (creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.upgrading = false;
             creep.say('🔄 collect');
-	    }
-	    if (!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
-	        creep.memory.upgrading = true;
-	        creep.say('⚡ upgrade');
-	    }
+        }
+        if (!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+            creep.memory.upgrading = true;
+            creep.say('⚡ upgrade');
+        }
 
-	    if(creep.memory.upgrading) {
+        if (creep.memory.upgrading) {
             if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+                creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
             }
         }
         else {
-            
+
+            //i should also use link
+            /*
             var sources = creep.room.find(FIND_STRUCTURES, { 
                 filter: (structure) => { 
                     return (((structure.structureType == STRUCTURE_STORAGE)
@@ -26,16 +28,53 @@ var roleUpgrader = {
                     structure.store[RESOURCE_ENERGY] > 0)
                 }
              });
-            
-             var source = creep.pos.findClosestByPath(sources);
-             //if i just upgraded the controller, i will use the closed source
-             //which most likely will be the storage
+             */
+
+            //decide dynamically what source of energy to use
+            //if storage or links exists - pick these randomly
+            //and ignore containers
+            //if only containers exist - use them
+            //worst case - go to source directly
+
+            var s_storage = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return ((structure.structureType == STRUCTURE_STORAGE) &&
+                        structure.store[RESOURCE_ENERGY] > 0)
+                }
+            });
+
+            var s_containers = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return ((structure.structureType == STRUCTURE_CONTAINER) &&
+                        structure.store[RESOURCE_ENERGY] > 0)
+                }
+            });
+
+            var s_link = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return ((structure.structureType == STRUCTURE_LINK) &&
+                        structure.store[RESOURCE_ENERGY] > 0 &&
+                        structure.pos.getRangeTo(structure.pos.findClosestByRange(FIND_STRUCTURES, {
+                            filter: (structure) => {
+                                return ((structure.structureType == STRUCTURE_STORAGE))
+                            }
+                        })) < 5) // this only return links that are close to storage
+                }
+            });
+
+
+            //TODO i should dynamically decide which source to use
+            //only use links if storeage and containers are not there
+            //TODO i should use links or containers here?
+            var source = creep.pos.findClosestByPath(_.merge(s_storage, s_link));
+
+            //find fullest source and use it?
 
             if (creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+                creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
             }
         }
-	}
+    }
 };
 
 module.exports = roleUpgrader;
